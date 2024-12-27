a9f0d523ba3bcf80d2194db2da51455d4990a35f
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-28 00:59:50 +0200

    Added bulletin service/dataProvider

---
+++ client/src/dataProvider/bulletin.js
@@ -0,0 +1,36 @@
+import { del, get, post } from '../libs/request';
+
+const BASE_URL = import.meta.env.VITE_API_URL;
+
+export async function addBulletin(bulletinData) {
+  try {
+    const response = await post(`${BASE_URL}/bulletin/subscribe`, bulletinData);
+    return response;
+  }
+  catch (error) {
+    console.error('Error subscribing:', error);
+    return [];
+  }
+}
+
+export async function getBulletins() {
+  try {
+    const response = await get(`${BASE_URL}/bulletin`);
+    return response;
+  }
+  catch (error) {
+    console.error('Error fetching bulletins:', error);
+    return [];
+  }
+}
+
+export async function removeBulletin(bulletinData) {
+  try {
+    const response = await del(`${BASE_URL}/bulletin/unsubscribe`, bulletinData);
+    return response;
+  }
+  catch (error) {
+    console.error('Error unsubscribing:', error);
+    return [];
+  }
+}
