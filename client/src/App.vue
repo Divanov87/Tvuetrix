b15a6944d93257f6ff1b6e4a8d5d0beafdac8f9d
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-27 21:32:14 +0200

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,10 +1,13 @@
 <script setup>
+import AppHeader from './components/layout/AppHeader/AppHeader.vue';
 import PWABadge from './components/PWABadge.vue';
 </script>
 
 <template>
+  <AppHeader />
   <PWABadge />
 </template>
 
 <style scoped>
+
 </style>
