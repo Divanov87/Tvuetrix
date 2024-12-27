2cf34c120fb4bb05816161daaee34bae3dcc3fde
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-28 01:49:22 +0200

    Fixed router misconfiguration

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,13 +1,16 @@
 <script setup>
-import AppFooter from './components/layout/AppFooter/AppFooter.vue';
 import AppHeader from './components/layout/AppHeader/AppHeader.vue';
+import AppFooter from './components/layout/AppFooter/AppFooter.vue';
 import PWABadge from './components/PWABadge.vue';
 </script>
 
 <template>
   <AppHeader />
-  <AppFooter />
+  <RouterView />
   <PWABadge />
+  <AppFooter />
 </template>
 
-<style scoped></style>
+<style scoped>
+
+</style>
--- client/src/main.js
+++ client/src/main.js
@@ -1,6 +1,7 @@
 import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
+import router from './configs/router';
 
 import { i18n } from './i18n';
 import './styles.css';
@@ -9,5 +10,8 @@ const pinia = createPinia();
 const app = createApp(App);
 
 app.use(i18n);
+app.use(router);
 app.use(pinia);
 app.mount('#app');
+
+
