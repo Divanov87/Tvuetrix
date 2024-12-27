fec2c8c3bfff11c656e6d78f984180a29116a14c
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-27 22:57:51 +0200

    Added authStore for handling auth

---
+++ client/src/store/authStore.js
@@ -0,0 +1,81 @@
+import Cookies from 'js-cookie';
+import { jwtDecode } from 'jwt-decode';
+import { defineStore } from 'pinia';
+import { computed, ref } from 'vue';
+import router from '../configs/router';
+import { loginService, logoutService, registerService } from '../dataProvider/user';
+
+export const useAuthStore = defineStore('auth', () => {
+  const token = ref(Cookies.get('user') || null);
+  const user = ref(null);
+
+  const isLogged = computed(() => !!token.value);
+
+  function setUserFromToken() {
+    const tokenValue = Cookies.get('user');
+    if (tokenValue) {
+      try {
+        user.value = jwtDecode(tokenValue);
+      }
+      catch (error) {
+        console.error('Failed to decode token', error);
+        user.value = null;
+        Cookies.remove('user');
+      }
+    }
+    else {
+      user.value = null;
+    }
+  }
+
+  async function login(username_email, password) {
+    try {
+      const userData = await loginService(username_email, password);
+      token.value = userData.token;
+      user.value = jwtDecode(userData.token);
+      Cookies.set('user', userData.token, { expires: 400 });
+    }
+    catch (error) {
+      console.error('Login failed', error);
+      return [];
+    }
+  }
+
+  async function register(username, password, repeatPassword, email, city) {
+    try {
+      const userData = await registerService(username, password, repeatPassword, email, city);
+      token.value = userData.token;
+      user.value = jwtDecode(userData.token);
+      Cookies.set('user', userData.token, { expires: 400 });
+    }
+    catch (error) {
+      console.error('Registration failed', error);
+      return [];
+    }
+  }
+
+  async function logout() {
+    try {
+      await logoutService();
+    }
+    catch (error) {
+      console.error('Logout failed', error);
+    }
+    finally {
+      Cookies.remove('user');
+      user.value = null;
+      token.value = null;
+      router.push('/');
+    }
+  }
+
+  return {
+    token,
+    user,
+    isLogged,
+    setUserFromToken,
+    login,
+    register,
+    logout,
+  };
+});
