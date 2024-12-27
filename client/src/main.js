c4bf3ce25afdddb2f67314384fbc9f0ed1372d0d
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-27 21:32:09 +0200

    Update main.js

--- client/src/main.js
+++ client/src/main.js
@@ -2,10 +2,12 @@ import { createPinia } from 'pinia';
 import { createApp } from 'vue';
 import App from './App.vue';
 
+import { i18n } from './i18n';
 import './styles.css';
 
 const pinia = createPinia();
 const app = createApp(App);
 
+app.use(i18n);
 app.use(pinia);
 app.mount('#app');
