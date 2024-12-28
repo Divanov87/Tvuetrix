9ddf2447b48906392bbc94a5448f0de52732b387
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-28 21:42:16 +0200

    Added MainPinned Dashboard component

---
+++ client/src/views/dashboard/event-item/components/main-pinned/MainPinned.vue
@@ -0,0 +1,37 @@
+<script setup>
+import Loader from '../../../../../components/Loader.vue';
+import EventCard from '../../../../events/components/event-card/EventCard.vue';
+
+defineProps({
+  latestPins: {
+    type: Array,
+    default: () => [],
+  },
+  isLoading: {
+    type: Boolean,
+    default: false,
+  },
+});
+</script>
+
+<template>
+  <section class="top-rated">
+    <div class="container">
+      <p class="section-subtitle">Latest</p>
+      <h2 class="h2 section-title">Pinned <strong>Events</strong></h2>
+      <div v-if="isLoading">
+        <Loader />
+      </div>
+      <ul v-else class="movies-list">
+        <template v-if="latestPins.length > 0">
+          <EventCard v-for="event in latestPins" :key="event._id" v-bind="event" />
+        </template>
+        <h2 v-else class="h2 section-title">There are no pinned events!</h2>
+      </ul>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+@import './MainPinned.css';
+</style>
