3656e21cb4bfecd4788f127d4382da5e27eb599f
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-28 23:29:59 +0200

    Update router.js

--- client/src/configs/router.js
+++ client/src/configs/router.js
@@ -1,6 +1,11 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+
+import Dashboard from '../views/dashboard/Dashboard.vue';
+
 const routes = [
+  { path: '/', component: Dashboard },
+
 ];
 
 const router = createRouter({
