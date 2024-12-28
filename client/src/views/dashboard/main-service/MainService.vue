51ec4f562497f24514a79e8ebec84768e4eb5b28
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-28 21:40:00 +0200

    Added MainService Dashboard component

---
+++ client/src/views/dashboard/main-service/MainService.vue
@@ -0,0 +1,61 @@
+<template>
+  <section id="service" class="service">
+    <div class="container">
+      <div class="service-banner">
+        <figure>
+          <img src="/assets/images/banner.jpg" alt="">
+        </figure>
+      </div>
+      <div class="service-content">
+        <p class="service-subtitle">
+          Our 3-steps to paradise:
+        </p>
+        <h2 class="h2 service-title">
+          Discover <strong>thousands</strong> of performances and <strong>events</strong>
+        </h2>
+        <p class="service-text">
+          Embark on a journey of endless entertainment as you explore and discover thousands of captivating
+          performances and events on our website!
+        </p>
+        <ul class="service-list">
+          <li>
+            <div class="service-card">
+              <div class="card-icon">
+                <i class="bx bxs-coupon" />
+              </div>
+              <div class="card-content">
+                <h3 class="h3 card-title">
+                  2. GET YOUR TICKETS
+                </h3>
+                <p class="card-text">
+                  Secure your front-row seat to unforgettable moments - buy tickets now and be part of the
+                  excitement with thousands of upcoming performances and events!
+                </p>
+              </div>
+            </div>
+          </li>
+          <li>
+            <div class="service-card">
+              <div class="card-icon">
+                <i class="bx bx-donate-heart" />
+              </div>
+              <div class="card-content">
+                <h3 class="h3 card-title">
+                  3. ENJOY
+                </h3>
+                <p class="card-text">
+                  Sit back, relax, and enjoy the spectacular performances and events unfold before you,
+                  creating moments of pure delight and unforgettable memories.
+                </p>
+              </div>
+            </div>
+          </li>
+        </ul>
+      </div>
+    </div>
+  </section>
+</template>
+
+<style scoped>
+@import './MainService.css';
+</style>
