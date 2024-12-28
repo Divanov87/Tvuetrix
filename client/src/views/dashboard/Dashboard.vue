fc0debc100de67cde26ad7b90387d734b3bbc325
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-28 21:40:13 +0200

    Update Dashboard.vue

--- client/src/views/dashboard/Dashboard.vue
+++ client/src/views/dashboard/Dashboard.vue
@@ -1,9 +1,11 @@
 <script setup>
+import MainService from './main-service/MainService.vue';
 import MainTop from './main-top/MainTop.vue';
 </script>
 
 <template>
   <div>
     <MainTop />
+    <MainService />
   </div>
 </template>
