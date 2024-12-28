c53743a9008e74862bcf9c9caeedf5b04eaba4a3
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-29 00:36:58 +0200

    Updated routes for Login and Register components

--- client/src/configs/router.js
+++ client/src/configs/router.js
@@ -1,11 +1,19 @@
 import { createRouter, createWebHistory } from 'vue-router';
 
+import Login from '../views/auth/Login.vue';
+import Register from '../views/auth/Register.vue';
 
 import Dashboard from '../views/dashboard/Dashboard.vue';
 
 const routes = [
-  { path: '/', component: Dashboard },
-
+  { path: '/', component: Dashboard, name: 'Dashboard' },
+  {
+    path: '/auth',
+    children: [
+      { path: 'login', component: Login, name: 'Login' },
+      { path: 'register', component: Register, name: 'Register' },
+    ],
+  },
 ];
 
 const router = createRouter({
