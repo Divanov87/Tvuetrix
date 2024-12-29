163c2efe2de7f8faf44ce8f8643941e675d8561c
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-29 20:37:49 +0200

    Added EventConcerts component

---
+++ client/src/views/events/event-concerts/EventConcerts.vue
@@ -0,0 +1,58 @@
+<script setup>
+import EventPagination from '../../../components/event-pagination/EventPagination.vue';
+import Loader from '../../../components/Loader.vue';
+import { useEvents } from '../../../composables/useEvents';
+import { usePagination } from '../../../composables/usePagination';
+import { usePaginationQuery } from '../../../composables/usePaginationQuery';
+import { getConcertEvents } from '../../../dataProvider/event';
+import EventCard from '../components/event-card/EventCard.vue';
+
+const { events, isLoading } = useEvents(getConcertEvents);
+const eventsPerPage = 8;
+const initialPage = usePaginationQuery(1);
+
+const { currentPage, totalPages, currentEvents, handlePageChange } = usePagination(
+  events,
+  eventsPerPage,
+  initialPage,
+);
+</script>
+
+<template>
+  <article>
+    <section class="top-rated">
+      <div class="container">
+        <p class="section-subtitle">
+          Latest
+        </p>
+        <h2 class="h2 section-title">
+          Music <strong>Concerts</strong>
+        </h2>
+
+        <Loader v-if="isLoading" />
+
+        <template v-else>
+          <ul class="movies-list">
+            <EventCard
+              v-for="event in currentEvents"
+              :key="event._id"
+              v-bind="event"
+            />
+            <h2 v-if="currentEvents.length === 0" class="h2 section-title">
+              No events found!
+            </h2>
+          </ul>
+          <EventPagination
+            :total-pages="totalPages"
+            :current-page="currentPage"
+            @page-change="handlePageChange"
+          />
+        </template>
+      </div>
+    </section>
+  </article>
+</template>
+
+<style scoped>
+@import '../../../views/search/Search.css';
+</style>
