6007b6fc57fb19cfe259d1aed5bfb73e4e2b2a03
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-29 20:38:31 +0200

    Updated router with EventTheater and EventConcerts components

--- client/src/configs/router.js
+++ client/src/configs/router.js
@@ -5,7 +5,9 @@ import Login from '../views/auth/Login.vue';
 import Register from '../views/auth/Register.vue';
 import Dashboard from '../views/dashboard/Dashboard.vue';
 import EventCatalog from '../views/events/event-catalog/EventCatalog.vue';
+import EventConcerts from '../views/events/event-concerts/EventConcerts.vue';
 import EventDetails from '../views/events/event-details/EventDetails.vue';
+import EventTheater from '../views/events/event-theater/EventTheater.vue';
 import Search from '../views/search/Search.vue';
 
 const routes = [
@@ -14,6 +16,8 @@ const routes = [
   {
     path: '/events',
     children: [
+      { path: 'theater', component: EventTheater, name: 'EventTheater' },
+      { path: 'concerts', component: EventConcerts, name: 'EventConcerts' },
       { path: ':eventId/details', component: EventDetails, name: 'EventDetails' },
     ],
   },
@@ -27,7 +31,6 @@ const routes = [
   { path: '/search', component: Search, name: 'Search' },
   { path: '/:pathMatch(.*)*', component: NotFound, name: 'NotFound' },
 ];
-
 
 const router = createRouter({
   history: createWebHistory(),
