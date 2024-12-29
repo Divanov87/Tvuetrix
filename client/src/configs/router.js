bc146fb6bdcfa476e5f87c48f0e556b327d749c8
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-29 20:12:59 +0200

    Updated router with EventCatalog component

--- client/src/configs/router.js
+++ client/src/configs/router.js
@@ -1,14 +1,15 @@
 import { createRouter, createWebHistory } from 'vue-router';
 import NotFound from '../components/not-found/NotFound.vue';
-import EventDetails from '../views/events/event-details/EventDetails.vue';
 
 import Login from '../views/auth/Login.vue';
 import Register from '../views/auth/Register.vue';
-
 import Dashboard from '../views/dashboard/Dashboard.vue';
+import EventCatalog from '../views/events/event-catalog/EventCatalog.vue';
+import EventDetails from '../views/events/event-details/EventDetails.vue';
 
 const routes = [
   { path: '/', component: Dashboard, name: 'Dashboard' },
+  { path: '/events', component: EventCatalog, name: 'EventCatalog' },
   {
     path: '/events',
     children: [
@@ -25,6 +26,7 @@ const routes = [
   },
   { path: '/:pathMatch(.*)*', component: NotFound, name: 'NotFound' },
 ];
+
 
 const router = createRouter({
   history: createWebHistory(),
