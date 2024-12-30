de225021fa2a9941bdf16a2944a8c31cf02a315d
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2024-12-30 20:02:06 +0200

    Added AdminPanel component

---
+++ client/src/views/admin-panel/AdminPanel.vue
@@ -0,0 +1,16 @@
+<script setup>
+import Bulletin from './components/Bulletin.vue';
+import Messages from './components/Messages.vue';
+import Users from './components/Users.vue';
+</script>
+
+
+<template>
+      <Users />
+      <Messages />
+      <Bulletin />
+</template>
+
+<style scoped>
+@import './AdminPanel.css';
+</style>
