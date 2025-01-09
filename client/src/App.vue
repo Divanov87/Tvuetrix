15fcdbdecfa6115ececed24db37f7b4081de5a1b
Dimitar.Ivanov87 <161376831+Divanov87@users.noreply.github.com>
2025-01-10 01:47:57 +0200

    Update App.vue

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,6 +1,6 @@
 <script setup>
-import AppHeader from './components/layout/AppHeader/AppHeader.vue';
 import AppFooter from './components/layout/AppFooter/AppFooter.vue';
+import AppHeader from './components/layout/AppHeader/AppHeader.vue';
 
 import PWABanner from './components/pwa-banner/PWABanner.vue';
 import PWABadge from './components/PWABadge.vue';
